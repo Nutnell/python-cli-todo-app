984d6d3378d14aff42b6d1346478a9b474ac9eb0
Nutnell <nutnell00@gmail.com>
2025-06-18 14:45:58 +0300

    Added a constructor for initializing a new Task object

---
+++ task.py
@@ -0,0 +1,30 @@
+from datetime import datetime, date
+
+
+class Task:
+    _assigned_id = 1
+
+
+def __init__(self, title, description="", due_date=None, completed=False, task_id=None):
+
+    if task_id is not None:
+        self.id = task_id
+
+    if task_id >= Task._assigned_id:
+        Task._assigned_id = task_id + 1
+    else:
+        self_id = Task._assigned_id
+        Task._assigned_id = + 1
+
+    self.title = title
+    self.description = description
+    self.completed = completed
+    
+    if due_date:
+      try:
+        self.due_date = datetime.strptime(due_date, "%Y-%m-%d").date()
+      except ValueError:
+        self.due_date=None
+        print(f"Warning: Invalid date format for task '{title}'")
+      else:
+        self.due_date=None

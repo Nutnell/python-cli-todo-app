6fd0eecc71b0ff7f2d38fc14d600f2106eb6d65f
Nutnell <nutnell00@gmail.com>
2025-06-19 04:48:02 +0300

    Introduced a test suite for the utility functions in utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,1 +1,10 @@
+import unittest
+import os
+import json
+from datetime import datetime, date
 
+from utils import load_tasks, save_tasks, TASKS_FILE
+from task import Task
+
+class TestUtils(unittest.TestCase):
+  
